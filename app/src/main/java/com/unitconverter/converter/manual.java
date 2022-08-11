8e85681c83548b6b2f92faacd653f37380aaffec
ElHadi73 <gggg68131@gmail.com>
2022-08-11 15:46:42 -0400

    jumping from the menu to the weights working and wights converter working

--- app/src/main/java/com/unitconverter/converter/manual.java
+++ app/src/main/java/com/unitconverter/converter/manual.java
@@ -1,6 +1,9 @@
 package com.unitconverter.converter;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import androidx.gridlayout.widget.GridLayout;
 
@@ -16,5 +19,13 @@ public class manual extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual);
 
+        final Button massButton = (Button) findViewById(R.id.button2);
+        massButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                        startActivity(new Intent(manual.this, wieghts.class));
+                        finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/unitconverter/converter/wieghts.java
+++ app/src/main/java/com/unitconverter/converter/wieghts.java
@@ -16,11 +16,11 @@ public class wieghts extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wieghts);
 
         valueinpounds = findViewById(R.id.textView7);
         editText = findViewById(R.id.editTextNumber);
-        editText.addTextChangedListener(new TextWatcher() {
+        TextWatcher textWatcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
             }
@@ -36,7 +36,8 @@ public class wieghts extends AppCompatActivity {
                 } catch (NumberFormatException emptyString) {
                 }
             }
-        });
+        };
+        editText.addTextChangedListener(textWatcher);
     }
 
     private void convert_kilo_to_pound() {
