204ed351e6b1948049060e9ccba08398b563469b
ElHadi73 <gggg68131@gmail.com>
2022-08-11 12:02:51 -0400

    added splach page , and jumps to a menu

--- app/src/main/java/com/unitconverter/converter/manual.java
+++ app/src/main/java/com/unitconverter/converter/manual.java
@@ -2,7 +2,7 @@ package com.unitconverter.converter;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.GridLayout;
+import androidx.gridlayout.widget.GridLayout;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class manual extends AppCompatActivity {
 
     GridLayout manualGrid;
-    Button button = new Button(this);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.manual);
-        manualGrid = findViewById(R.id.gridlayout1);
-        manualGrid.addView(button);
+        setContentView(R.layout.activity_manual);
+
     }
 }
